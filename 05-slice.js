c8e8f3a068cb0e5effbb7520a385f7f13f47ff91
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-05-19 18:00:16 +0700

    Slice method update

--- 05-slice.js
+++ 05-slice.js
@@ -1,6 +1,10 @@
-const animals = ["Cat", "Dog", "Horse", "Sheep", "Cow"];
+const animals = ["Cat", "Dog", "Horse", "Sheep", "Cow", "Goat", "Ant", "Lion"];
 console.log(animals);
 
-const newAnimals = animals.slice(1,3);
+const newAnimals = animals.slice(1, 3);
 
 console.log(newAnimals);
+
+const nextAnimals = animals.slice(1, -4)
+
+console.log(nextAnimals);
