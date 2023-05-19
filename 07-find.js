2b544f472c42cba452930248263c48928b356e05
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-05-19 18:28:14 +0700

    Find method

---
+++ 07-find.js
@@ -0,0 +1,7 @@
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const lower = numbers.find(low => low < 6);
+
+console.log(lower); // 2 Because find methods only return the first element that less than the refference value
+
+
